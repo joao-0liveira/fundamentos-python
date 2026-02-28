12495356fcb3242dbd3cb8985dcea53f79c366e5
joao-0liveira <oliveira2008joao@gmail.com>
2026-02-28 00:24:02 -0300

    Aprimoramento: Correção de erros e novas funcionalidades

--- Codes/desafios/Desafio 51.py
+++ Codes/desafios/Desafio 51.py
@@ -5,7 +5,8 @@ init(autoreset=False)
 colors = {
     'blue': Fore.BLUE,
     'yellow': Fore.YELLOW,
-    'magenta': Fore.MAGENTA
+    'magenta': Fore.MAGENTA,
+    'red': Fore.RED
 }
 
 print(f'{Style.BRIGHT}-=-'*6)
@@ -25,8 +26,22 @@ expressao = ''
 
 while contador < 10:
     expressao = expressao + f'{soma} + '
-    soma = soma + termo1
+    soma = soma + razao           #correção da progressão
     contador = contador + 1
 
 print(f'''\nA progressão que inicia-se no termo: {colors['blue']}{termo1}{Style.RESET_ALL}, e de razão: {colors['blue']}{razao}{Style.RESET_ALL},
-pode ser escrita da seguinte forma: {Style.BRIGHT}{colors['yellow']}{expressao}{termo1*10}.''')
+pode ser escrita da seguinte forma: {Style.BRIGHT}{colors['yellow']}{expressao}{termo1*10}{Style.RESET_ALL}.''')
+
+#aprimoramento se o usuário quiser escolher um novo número de termos para a progressão
+pergunta = str(input(f'\nDigite {colors['blue']}[S]{Style.RESET_ALL} para escolher um outro número de termos para essa progressão ou {colors['red']}[N]{Style.RESET_ALL} para finalizar o programa: ')).lower()
+
+if pergunta == 'n':
+    print('\nTudo bem! Programa finalizado!')
+else:
+    termos = int(input('\nÓtimo! nos informe qual é o número de termos que você deseja: '))
+    while contador < termos:
+        expressao = expressao + f'{soma} + '
+        soma = soma + termo1
+        contador = contador + 1
+    print(f'''\nA nova progressão que inicia-se no termo: {colors['blue']}{termo1}{Style.RESET_ALL}, e de razão: {colors['blue']}{razao}{Style.RESET_ALL},
+pode ser escrita da seguinte forma: {Style.BRIGHT}{colors['yellow']}{expressao}{termo1*termos}{Style.RESET_ALL}.''')
